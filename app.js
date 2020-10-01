17716273565f4e287498547a7178a473c479fabb
dejan-77 <dejankutic@me.com>
2020-10-01 17:22:55 +0200

    bez getter

--- app.js
+++ app.js
@@ -202,32 +202,3 @@ $('.swiper-container').on('mouseup touchend', function(event) {
 });
 
 
-// CUSTOM BOTTUN GETTER SETTER
-gsap.set(".customplay", {xPercent: -50, yPercent: -50});
-
-var ball = document.querySelector(".customplay");
-var pos = { x: window.innerWidth / 2, y: window.innerHeight / 2 };
-var mouse = { x: pos.x, y: pos.y };
-var speed = 0.1;
-
-var fpms = 60 / 1000;
-
-var xSet = gsap.quickSetter(ball, "x", "px");
-var ySet = gsap.quickSetter(ball, "y", "px");
-
-window.addEventListener("mousemove", e => {    
-  mouse.x = e.x;
-  mouse.y = e.y;  
-});
-
-gsap.ticker.add((time, deltaTime) => {
-  
-  var delta = deltaTime * fpms;
-  var dt = 1.0 - Math.pow(1.0 - speed, delta); 
-  
-  pos.x += (mouse.x - pos.x) * dt;
-  pos.y += (mouse.y - pos.y) * dt;
-  xSet(pos.x);
-  ySet(pos.y);
-});
-
