95e402841034d484860c59ba5ef1c01b6514dbb4
dejan-77 <dejankutic@me.com>
2020-10-01 16:59:11 +0200

    lottie

--- app.js
+++ app.js
@@ -271,3 +271,74 @@ gsap.ticker.add((time, deltaTime) => {
   ySet(pos.y);
 });
 
+// -- VIDEK SCROLL LOTTIE ------------------
+
+class ScrubControlledAnimation {
+  constructor() {
+    this.DOM = {
+      animationWrapper: ".js-scrub-controlled-animation-wrapper",
+      animation: ".js-scrub-controlled-animation",
+      states: {}
+    };
+
+    this.animationWrapper = document.querySelector(this.DOM.animationWrapper);
+
+    this.animation = document.querySelector(this.DOM.animation);
+
+    this.init();
+  }
+
+  init() {
+    console.log("ScrubControlledAnimation init()");
+
+    if (this.animation) {
+      this.scrubAnimation();
+    }
+  }
+
+  scrubAnimation() {
+    const scrubAnimationOptions = {
+      container: this.animation,
+      renderer: "svg",
+      loop: true,
+      autoplay: false,
+      path: this.animation.getAttribute("data-animation-source")
+    };
+
+    /**
+     *
+     */
+    let scrubAnimation = lottie.loadAnimation(scrubAnimationOptions);
+
+    scrubAnimation.addEventListener("DOMLoaded", () => {
+      this.loadAnimation(scrubAnimation);
+    });
+  }
+
+  loadAnimation(animation) {
+    const scrubAnimationTimeline = gsap.timeline({}).to(
+      { frame: 0 },
+      {
+        duration: 1,
+        frame: animation.totalFrames - 1,
+        onUpdate: function () {
+          animation.goToAndStop(Math.round(this.targets()[0].frame), true);
+        }
+      },
+      "start"
+    );
+
+    ScrollTrigger.create({
+      trigger: this.animationWrapper,
+      animation: scrubAnimationTimeline,
+       scroller: ".smooth-scroll",
+      // markers: true,
+      pin:"#kingpin",
+      start: "top top",
+      end: "bottom top",
+      scrub: 0.4
+    });
+  }
+}
+
+new ScrubControlledAnimation();
