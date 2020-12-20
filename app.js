b0d8549a2e59b3ca17bb7116abd1269bb1364243
dejan-77 <dejankutic@me.com>
2020-12-20 16:29:19 +0100

    aj

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ function show() {
 		.from(".small-link", {x:-40, stagger:0.1, opacity:0, duration:0.3, ease: "power1.out"}, "<")
 		.from(".wg-element-wrapper", {opacity:0, duration:0.3}, "<0.5")
     
-   .set(".nav-wrapper, .nav-toggle", {pointerEvents: "all"}, "<")
+    .set(".nav-wrapper, .nav-toggle", {pointerEvents: "all"}, "<")
 
 }
 // --- SHOW
@@ -64,6 +64,32 @@ function hide() {
 	
 }
 
+
+//-------
+// SUPERSCRIPT MOUSEOVER // ISTA ANIMACIJA VIŠE ELEMENATA / LOOP
+// loop through each element
+$(".link-wrapper").each(function(i, el) {
+// set some individual properties
+//TweenMax.set($(el).find('.back'), {backgroundColor:'#' + Math.floor(Math.random() * 16777215).toString(16)});
+  
+// create a timeline for this element in paused state
+  var tl = gsap.timeline({paused: true});
+  // create your tween of the timeline in a variable
+  var t = tl
+         //.set(el,{willChange:"transform"})
+         //.set($(el).find('.wrap'), {zIndex: 2, overwrite:"all"})
+         .from($(el).find('.superscript'), {y:"20px", autoAlpha:0, duration: 0.3, overwrite:"all", ease: "power1.out"})
+         .to($(el).find('.main-link'), {duration: 0.3, overwrite:"all", ease: "power1.out"}, "<");
+  // store the tween timeline in the javascript DOM node
+  el.animation = t;
+  //create the event handler
+  $(el).on("mouseenter",function(){
+    this.animation.play();
+  }).on("mouseleave",function(){
+    this.animation.reverse();
+  });
+});
+//-------
 
 //-------
 // SUBMENU - CHANGE COLOR HOVER / LOOP / ista skripta ko ova poviše ali bez komentara
@@ -96,6 +122,47 @@ $(el).on("mouseenter",function(){
 });
 //-------
 
+// UNDERLINE
+// Mouseenter function
+function enterAnimation(link, e, index) {
+  link.tl.tweenFromTo(0, "midway");
+}
+// Mouseleave function
+function leaveAnimation(link, e) {
+  link.tl.play();
+}
+// Animations variables
+let workLinkUnderlineAnimEnter;
+let workLinkUnderlineAnimLeave;
+
+// Get all links
+let workLinks = document.querySelectorAll(".link-wrapper");
+
+workLinks.forEach((link, index, value) => {
+  
+  let underline = link.querySelector(".underline");
+    link.tl = gsap.timeline({paused: true});
+  
+  link.tl.fromTo(underline, {width: "0%", left: "0%",}, 
+  {width: "100%", duration: 0.3, ease: "power1.out",});
+  		
+  link.tl.add("midway");
+  
+  link.tl.fromTo(underline, {width: "100%", left: "0%",}, 
+  {width: "0%", left: "100%", duration: 0.3, ease: "power1.in", immediateRender: false});
+
+  // Mouseenter
+  link.addEventListener("mouseenter", (e) => {
+    enterAnimation(link, e, index);
+  });
+
+  // Mouseleave
+  link.addEventListener("mouseleave", (e) => {
+    leaveAnimation(link, e);
+  });
+
+});
+ 
 
 	// MENU ICON MOUSEOVER 
 menuhover.addEventListener('mouseover', ()=> {  
@@ -128,10 +195,9 @@ closemenu.addEventListener("click", function(){aniout.restart(), aniout.play(); 
 // --- 017 - LOCOMOTIVE SCROLL TO  --------------------------------------------------------------------------
 
 
-// --- 017 - LOCOMOTIVE 4.0 SCROLL TO  ------
-//--------------------------------------------------------------------
+// --- 017 - LOCOMOTIVE 4.0 SCROLL TO  --------------------------------------------------------------------------
 $( "#totop" ).on( "click", function() {
-	locoScroll.scrollTo( '#startx', {
+	locoScroll.scrollTo( '#start', {
 		'offset': 0,
 		'duration': 1000,
 		//'easing': [0.25, 0.00, 0.35, 1.00],
