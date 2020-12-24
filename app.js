717b9f5f16529b6d272acb6d135e29473ba35825
dejan-77 <dejankutic@me.com>
2020-12-24 13:23:20 +0100

    bella

--- app.js
+++ app.js
@@ -1,8 +1,220 @@
 
+const select = (e) => document.querySelector(e);
+const selectAll = (e) => document.querySelectorAll(e);
+
+const sections = selectAll('.rg__column');
+
+// create hover effect for each portfolio navigation item
+const allLinks = gsap.utils.toArray('.portfolio__categories a');
+const pageBackground = select('.fill-background');
+const largeImage = select('.portfolio__image--l');
+const smallImage = select('.portfolio__image--s');
+const lInside = select('.portfolio__image--l .image_inside');
+const sInside = select('.portfolio__image--s .image_inside');
+
+const loader = select('.loader');
+const loaderInner = select('.loader .inner');
+const progressBar = select('.loader .progress');
+const loaderMask = select('.loader__mask');
+
+// images loaded
+function init(){
+
+    // show loader on page load
+    gsap.set(loader, {autoAlpha: 1});
+
+    // scale loader down
+    gsap.set(loaderInner, {scaleY: 0.005, transformOrigin: 'bottom'});
+
+    // make a tween that scales the loader
+    const progressTween = gsap.to(progressBar, {paused: true, scaleX: 0, ease: 'none', transformOrigin: 'right'});
+
+    // setup variables
+    // https://codepen.io/desandro/pen/hlzaw
+    let loadedImageCount = 0, imageCount;
+    const container = select('#main');
+
+    // setup Images loaded
+    const imgLoad = imagesLoaded( container );
+    imageCount = imgLoad.images.length;
+
+    // set the initial progress to 0
+    updateProgress(0);
+
+    // triggered after each item is loaded
+    imgLoad.on( 'progress', function() {
+        // increase the number of loaded images
+        loadedImageCount++;
+        // update progress
+        updateProgress( loadedImageCount );
+    });
+
+    // update the progress of our progressBar tween
+    function updateProgress( value ) {
+        // console.log(value/imageCount)
+        // tween progress bar tween to the right value
+        gsap.to(progressTween, {progress: value/imageCount, duration: 0.3, ease: 'power1.out'})
+    }
+
+    // do whatever you want when all images are loaded
+    imgLoad.on( 'done', function( instance ) {
+        // we will simply init our loader animation onComplete
+        gsap.set(progressBar, {autoAlpha: 0, onComplete: initPageTransitions});
+    });
+
+}
+
+init();
+
+function pageTransitionIn({container}) {
+    // console.log('pageTransitionIn');
+    // timeline to stretch the loader over the whole screen
+    const tl = gsap.timeline({
+        defaults: {
+            duration: 0.8,
+            ease: 'power1.inOut'
+        }
+    });
+    tl
+        .set(loaderInner, { autoAlpha: 0 })
+        .fromTo(loader, { yPercent: -100 }, {yPercent: 0 })
+        .fromTo(loaderMask, { yPercent: 80 }, {yPercent: 0 }, 0)
+        .to(container, { y: 150}, 0);
+    return tl;
+}
+
+function pageTransitionOut({container}) {
+    // console.log('pageTransitionOut');
+    // timeline to move loader away down
+    const tl = gsap.timeline({
+        defaults: {
+            duration: 0.8,
+            ease: 'power1.inOut'
+        },
+        onComplete: () => initContent()
+    });
+    tl
+        .to(loader, { yPercent: 100 })
+        .to(loaderMask, { yPercent: -80 }, 0)
+        .from(container, { y: -150}, 0);
+    return tl;
+}
+
+function initPageTransitions() {
+    // do something before the transition starts
+    barba.hooks.before(() => {
+        select('html').classList.add('is-transitioning');
+    });
+    // do something after the transition finishes
+    barba.hooks.after(() => {
+        select('html').classList.remove('is-transitioning');
+    });
+
+    // scroll to the top of the page
+    barba.hooks.enter(() => {
+        window.scrollTo(0, 0);
+    });
+
+    barba.init({
+        transitions: [{
+            once() {
+                // do something once on the initial page load
+                initLoader();
+            },
+            async leave({current}) {
+                // animate loading screen in
+                await pageTransitionIn(current);
+            },
+            enter({next}) {
+                // animate loading screen away
+                pageTransitionOut(next);
+            }
+        }]
+    });
+}
+
+function initLoader() {
+    
+    const tlLoaderIn = gsap.timeline({
+        id: 'tlLoaderIn',
+        defaults: {
+            duration: 1.1,
+            ease: 'power2.out'
+        },
+        onComplete: () => initContent()
+    });
+
+    const image = select('.loader__image img');
+    const mask = select('.loader__image--mask');
+    const line1 = select('.loader__title--mask:nth-child(1) span');
+    const line2 = select('.loader__title--mask:nth-child(2) span');
+    const lines = selectAll('.loader__title--mask');
+    const loaderContent = select('.loader__content');
+
+    tlLoaderIn
+        .set(loaderContent, {autoAlpha: 1})
+        .to(loaderInner, {
+            scaleY: 1,
+            transformOrigin: 'bottom',
+            ease: 'power1.inOut'
+        })
+        .addLabel('revealImage')
+        .from(mask, {yPercent: 100}, 'revealImage-=0.6')
+        .from(image, {yPercent: -80}, 'revealImage-=0.6')
+        .from([line1, line2], {yPercent: 100, stagger: 0.1}, 'revealImage-=0.4');
+
+    const tlLoaderOut = gsap.timeline({
+        id: 'tlLoaderOut',
+        defaults: {
+            duration: 1.2,
+            ease: 'power2.inOut'
+        },
+        delay: 1
+    });
+    
+    tlLoaderOut
+        .to(lines, {yPercent: -500, stagger: 0.2}, 0)
+        .to([loader, loaderContent], {yPercent: -100}, 0.2)
+        .from('#main', {y: 150}, 0.2);
+
+    const tlLoader = gsap.timeline();
+    tlLoader
+        .add(tlLoaderIn)
+        .add(tlLoaderOut);
+
+}
+
+
+
+function initContent() {
+
+    select('body').classList.remove('is-loading');
+    initSmoothScrollbar();
+    initNavigation();
+    initHeaderTilt();
+    initHoverReveal();
+    initPortfolioHover();
+    initImageParallax();
+    initPinSteps();
+    initScrollTo();
+
+}
+
+const updateBodyColor = (color) => {
+    // gsap.to('.fill-background', { backgroundColor: color, ease: 'none'});
+    document.documentElement.style.setProperty('--bcg-fill-color', color);
+}
+const getTextHeight = (textCopy) => {
+    return textCopy.clientHeight;
+}
+
+
+
 // provjeri, ali mislim da ode idu svi ovi scroll trigger init i smoothscroll
 
 
-function init(){
+
+//function init(){
     
   // start here
 
@@ -235,13 +447,13 @@ gsap.to( $(".swiper-slide"),
 
 
 
-}
+/*}
 
 window.addEventListener('load', function(){
   init();
 });
 
-
+*/
 
 
 
