5b9a9b0432d2c26c02dbb4dabb7598bd54ec2bd6
dejan-77 <dejankutic@me.com>
2020-12-24 18:37:10 +0100

    init loader

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-gsap.registerPlugin(ScrollTrigger);
+
 
 // --- SMOOTH SCROLL -----------------------------------------
 
@@ -58,55 +58,10 @@ gsap.timeline({
 
 // ------ dovde je meni i js iz solo pagea
 
+gsap.registerPlugin(ScrollTrigger);
 
 
 
-
-// Smoooth Scrollbar
-function initSmoothScrollbar() {
-    
-
-  // brisi
-
-
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-const locoScroll = new LocomotiveScroll({
-el: document.querySelector(".smooth-scroll"),
-smooth: true,
-getDirection: true,
-scrollFromAnywhere: true,
-//smoothMobile: true,
-//lerp: .05
-}); 
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the ".smooth-scroll" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".smooth-scroll", {
-scrollTop(value) {
-  return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-}, // we don't have to define a scrollLeft because we're only scrolling vertically.
-getBoundingClientRect() {
-  return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-},
-
-// LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, 
-// we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-// UKLJUČITI SAMO NA MOBILNOJ VERZIJI
-// pinType: document.querySelector(".smooth-scroll").style.transform ? "transform" : "fixed"
-});
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-
-
-  // brisi 
-
-  
-}
 
 
 const select = (e) => document.querySelector(e);
@@ -300,6 +255,7 @@ function initContent() {
 
     select('body').classList.remove('is-loading');
     initSmoothScrollbar();
+    initLoader();
     //initNavigation();
     //initHeaderTilt();
     initHoverReveal();
