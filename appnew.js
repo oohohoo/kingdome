37a2afe1574c99dc618c6fb67de73ecd41084cfe
dejan-77 <dejankutic@me.com>
2020-12-30 16:05:06 +0100

    dfff

--- appnew.js
+++ appnew.js
@@ -383,12 +383,28 @@ BARBA VIEWS
 ================================================================================
 */  
   views: [{
+    namespace: 'home',
+    beforeEnter(data) {
+      console.log("Home JS triggered!");
+    }},{
     namespace: 'products',
     beforeEnter(){
       productsMainSwiper();
-        console.log("Products swiper triggered!");
-    } 
-
+        console.log("Products JS triggered!");
+    }},{
+      namespace: 'productlottie',
+    beforeEnter(data) {
+      console.log("Productlottie JS triggered!");
+    }},{
+    namespace: 'about',
+    beforeEnter(data) {
+      console.log("about JS triggered!");
+    }},{
+    namespace: 'contact',
+    beforeEnter(data) {
+      console.log("contact JS triggered!");
+      
+    }
 
 }],
 /*
@@ -823,6 +839,51 @@ $( "#totop" ).on( "click", function() {
 /*
 ================================================================================
 PRODUCTS - FULLSCREEN SWIPER
+================================================================================
+*/
+function productsMainSwiper() {
+
+  const slider = document.getElementById("js-cta-slider");
+  const sliderCounter = document.getElementById("js-cta-slider-counter");
+  const sliderNext = document.getElementById("js-cta-slider-next");
+  const sliderPrevious = document.getElementById("js-cta-slider-previous");
+  
+  const interleaveOffset = 0.75;
+  
+    
+  
+  // svaka fotka ima: data-swiper-parallax-y: "35%"
+  
+  const swiper = new Swiper(slider, {
+    autoplay: false,
+    parallax: true,
+    loop: true,
+    effect: "slide",
+    direction: "vertical", // put horizontal
+    speed: 1000,
+    grabCursor: true,
+    watchSlidesProgress: true, // turn off for horizontal
+    //mousewheelControl: true,
+    mousewheelControl: 1,
+    mousewheel: true,
+    pagination: {
+      el: sliderCounter,
+      type: "custom",
+      renderCustom: function(swiper, current, total) {
+        let i = current ? current : 0;
+        return `${("0" + i).slice(-2)} / ${("0" + total).slice(-2)}`;
+      }
+    },
+    navigation: {
+      nextEl: sliderNext,
+      prevEl: sliderPrevious
+    },
+  });
+}
+
+/*
+================================================================================
+PRODUCTS - SOLO PRODUCT LOTTIE
 ================================================================================
 */
 function productsMainSwiper() {
---
+++ old_product-single.js
@@ -0,0 +1,175 @@
+
+
+
+// --- 011 - ACCORDION --------------------------------------------------------------------------	
+
+var animations = $(".accordion-group").map(createAnimation);
+
+$(".accordion-menu").click(playAnimation);
+
+function playAnimation(event) {
+  
+  var selected = this;
+  
+  animations.each(function(i, animate) {
+    animate(selected);
+  });
+}
+
+function createAnimation(i, element) {
+    
+  var menu = element.querySelector(".accordion-menu");
+  var box  = element.querySelector(".accordion-content");
+  
+  gsap.set(box, { height: "auto"})
+  var tween = gsap.from(box, { duration:0.5, height: 0, ease: Power1.easeInOut }).reverse();
+  
+  return function(selected) {
+    
+    var reversed = selected !== menu ? true : !tween.reversed();
+    tween.reversed(reversed);
+  }
+}
+
+
+
+
+// -- VIDEK SCROLL LOTTIE
+
+class ScrubControlledAnimation {
+  constructor() {
+    this.DOM = {
+      animationWrapper: ".js-scrub-controlled-animation-wrapper",
+      animation: ".js-scrub-controlled-animation",
+      states: {}
+    };
+
+    this.animationWrapper = document.querySelector(this.DOM.animationWrapper);
+
+    this.animation = document.querySelector(this.DOM.animation);
+
+    this.init();
+  }
+
+  init() {
+    console.log("ScrubControlledAnimation init()");
+
+    if (this.animation) {
+      this.scrubAnimation();
+    }
+  }
+
+  scrubAnimation() {
+    const scrubAnimationOptions = {
+      container: this.animation,
+      renderer: "svg",
+      loop: true,
+      autoplay: false,
+      path: this.animation.getAttribute("data-animation-source")
+    };
+
+    /**
+     *
+     */
+    let scrubAnimation = lottie.loadAnimation(scrubAnimationOptions);
+
+    scrubAnimation.addEventListener("DOMLoaded", () => {
+      this.loadAnimation(scrubAnimation);
+    });
+  }
+
+  loadAnimation(animation) {
+    const scrubAnimationTimeline = gsap.timeline({}).to(
+      { frame: 0 },
+      {
+        duration: 1,
+        frame: animation.totalFrames - 1,
+        onUpdate: function () {
+          animation.goToAndStop(Math.round(this.targets()[0].frame), true);
+        }
+      },
+      "start"
+    );
+
+    ScrollTrigger.create({
+      trigger: this.animationWrapper,
+      animation: scrubAnimationTimeline,
+       scroller: ".smooth-scroll",
+      // markers: true,
+      pin:"#kingpin",
+      start: "top 20%",
+      end: "bottom top",
+      scrub: 0.4
+    });
+  }
+}
+
+new ScrubControlledAnimation();
+
+// --- LOGO ANIMACIJA 
+
+gsap.timeline({
+  scrollTrigger: {
+     scroller: ".smooth-scroll",
+			trigger: "#logotrigger",
+      start: "top top", // when the top of the trigger hits the top of the viewport
+      end: "+=10000000", // end after scrolling 500px beyond the start
+			toggleActions: 'play reverse play reverse',
+      invalidateOnRefresh: true,
+
+  }
+})
+.to(".red-flag",  {width:'4em', height:'4em', top: '-1.25em', duration: 0.5, ease: "expo.inOut", })
+.to(".znak", { scale: 0.6, transformOrigin: 'center center', yPercent: -55, ease:'expo.inOut'}, "<")
+
+
+// -- ELIPSE PIN
+ScrollTrigger.create({
+scroller: ".smooth-scroll",
+  trigger: "#elipsepin",
+  start: "top 30%", 
+//  end: "bottom 30%",
+ end: "+=90%",
+  pin: ".elipse",
+   invalidateOnRefresh: true,
+});
+
+/*
+gsap.timeline({
+  scrollTrigger: {
+     scroller: ".smooth-scroll",
+			trigger: "#elipsepin",
+       start: "top 30%", 
+      end: "+=90%",
+      pin: ".elipse-wrapper",
+			toggleActions: 'play reverse play reverse',
+      invalidateOnRefresh: true,
+
+  }
+})
+*/
+//.to(".elipse",  {autoAlpha:0, duration: 0.5, ease: "expo.inOut", })
+//.to(".znak", { scale: 0.6, transformOrigin: 'center center', yPercent: -55, ease:'expo.inOut'}, "<")
+
+
+
+
+
+
+
+
+/*
+
+// LOTTIE SCROLL FOR ROTATION
+ScrollLottie({
+ target: '#lottiewindow',
+ scroller: ".smooth-scroll",
+ path: 'https://uploads-ssl.webflow.com/5ecf72e4a8033653b5813402/5f26c88bac3de0b645af52b8_apple.json', 
+ duration: 8, 
+ speed: 'medium'
+})
+
+*/
+
+// --- 
+
