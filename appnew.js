28175038c5c91961f8b3c56f213c7af614ae39ed
dejan-77 <dejankutic@me.com>
2021-01-01 23:28:19 +0100

    vdf

--- appnew.js
+++ appnew.js
@@ -1188,6 +1188,7 @@ function simpleTicker() {
     'INNOVATIVE LUXURY CAMPING DOMES',
     'SOME OTHER NEWS',
     'HELLO WORLD',
+    'FREE DELIVERY ON ORDERS OVER $100<del style="color:yellow;">FOO/del>',
     ];
     
     $('#demo_3').t(
