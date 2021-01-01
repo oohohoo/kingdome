68df42d1ade2c434b60f97787cac32af430c2ff0
dejan-77 <dejankutic@me.com>
2021-01-01 17:32:07 +0100

    dd

--- appnew.js
+++ appnew.js
@@ -1166,11 +1166,10 @@ REINIT BACKGROUND VIDEOS
 function initVideo() {
   const select = (e) => document.querySelector(e);
 /*  // let video = body.querySelector('.background-video'); */
- let video = select('.background-video');
+ const video = select('.background-video');
   
- 
 video.setAttribute('autoplay', true);
-video.load();
+//video.load();
 
   console.log("BACKGROOUND VIDEOS RELOADED");
 
