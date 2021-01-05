b3dc46030cf07b664577f6b89f272d72a3655349
dejan-77 <dejankutic@me.com>
2021-01-05 19:00:19 +0100

    grgr

--- appnew.js
+++ appnew.js
@@ -1274,8 +1274,8 @@ function resetLogo() {
   let tl = gsap.timeline();
 
     tl.to(".red-flag",  {width:'4em', height:'4em', top: '-1.25em', duration: 0.5, ease: "expo.inOut", })
-    .fromTo(".red-flag", {clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 80%, 0% 100%)"}, 
-    {duration:0.5, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%)"}, "<")
+    .fromTo(".red-flag", {clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%)"}, 
+    {duration:0.5, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 80%, 0% 100%)"}, "<")
     .to(".znak", { scale: 0.6, transformOrigin: 'center center', yPercent: -55, ease:'expo.inOut'}, "<")
       
     console.log("logoReseted");
