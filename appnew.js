d90b8f131d2a6e633b198dc4c92bc409e6591cc3
dejan-77 <dejankutic@me.com>
2021-01-05 17:50:03 +0100

    gfhž

--- appnew.js
+++ appnew.js
@@ -821,7 +821,7 @@ function show() {
     .to(".fs-menu--column", {yPercent:0, duration:0.6, ease: "power2.inOut"}, "<")
     .to(".fs-menu-header", {yPercent:0, duration:0.8, ease: "power2.out"}, "<0.1")
    
-    .to(".link-wrapper", {autoAlpha:1, duration:0.6,stagger:0.1,  ease: "power2.inOut"}, "<0.1")
+    .to(".link-wrapper", {autoAlpha:1, duration:0.5,stagger:0.1,  ease: "power2.inOut"}, "<0.1")
     .to(".fadein", {autoAlpha:1, duration:0.6, ease: "power2.inOut"}, "<0.1")
 
 
