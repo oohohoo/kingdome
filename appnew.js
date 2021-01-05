182b754365c52e80d4ae91607288b3c3329db4b8
dejan-77 <dejankutic@me.com>
2021-01-05 20:33:41 +0100

    dddd

--- appnew.js
+++ appnew.js
@@ -176,7 +176,6 @@ SCROLLTRIGGER TEST
 
 
   // SCROLLTRIGGER TEST 2 - LOGO ANIMACIJA TEST TEST
-  gsap.set(".red-flag",  {clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 80%, 0% 100%)"})
 
   gsap.timeline({
     scrollTrigger: {
@@ -193,8 +192,7 @@ SCROLLTRIGGER TEST
   .fromTo(".red-flag", {clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 80%, 0% 100%)"}, 
 {duration:0.5, clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 100%, 0% 100%)"}, "<")
   .to(".znak", { scale: 0.6, transformOrigin: 'center center', yPercent: -55, ease:'expo.inOut'}, 0)
- 
- .set(".red-flag",  {clipPath:"polygon(0% 0%, 100% 0%, 100% 100%, 50% 80%, 0% 100%)"})
+
 
 
   /*
