744e36dbad1c258585a99d2c9110e0039c944f0f
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-01 18:16:47 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -412,7 +412,7 @@ BARBA VIEWS
       homeProductHover();
       simpleTicker();
       simpleTickerShow(); */
-      homeAboutSlider();
+      /* homeAboutSlider(); */
      // document.getElementById('video').play();
       
 
