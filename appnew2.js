22b43dcaec5347913c633d0d25d9885cefaf8c81
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-02 17:38:30 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -203,11 +203,12 @@ LOGO ANIMACIJA
 
 
 
+
   gsap.timeline({
     scrollTrigger: {
        scroller: ".smooth-scroll",
         trigger: "#start",
-        markers: true,
+        /* markers: true, */
         start: "top 5%", // when the top of the trigger hits the top of the viewport
         end: "+=10000000", // end after scrolling 500px beyond the start
         toggleActions: 'play reverse play reverse',
@@ -217,7 +218,7 @@ LOGO ANIMACIJA
   })
 
   .to("#di",  {width:'4em', height:'4em', top: '-1.25em', duration: 0.5, ease: "expo.inOut", }, 0)
-  .to("#di", {morphSVG: {shape: "#sq"},duration: 0.5}, "<")
+  .to("#di", {morphSVG: {shape: "#sq"},duration: 0.8}, "<")
 
  /*  .to("#di", {morphSVG: {shape: "#sq"},duration: 0.8, repeat: -1, yoyo: true, repeatDelay: 1}, "<") */
   /*
@@ -226,7 +227,7 @@ LOGO ANIMACIJA
 
 
   .to(".header-znak", { scale: 0.6, transformOrigin: 'center center', yPercent: -55, ease:'expo.inOut'}, 0)
- /*  .to("#ticker",  {autoAlpha:0, duration: 0.1}); */
+  .to("#ticker",  {autoAlpha:0, duration: 0.1});
 
 
   /*
