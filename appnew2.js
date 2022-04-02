9af196271e627ed8c2d9b76be824c558e623faac
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-02 17:41:28 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -217,7 +217,7 @@ LOGO ANIMACIJA
     }
   })
 
-  .to(".header_redflag",  {width:'4rem', height:'4rem', top: '-1.25em', duration: 0.5, ease: "expo.inOut", }, 0)
+  .to(".header_redflag",  {width:'4rem', height:'4rem', top: '-1.25rem', duration: 0.5, ease: "expo.inOut", }, 0)
   .to("#di", {morphSVG: {shape: "#sq"},duration: 0.8}, "<")
 
  /*  .to("#di", {morphSVG: {shape: "#sq"},duration: 0.8, repeat: -1, yoyo: true, repeatDelay: 1}, "<") */
