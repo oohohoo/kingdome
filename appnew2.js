8ebee94e24f724eeddf559e0a8a28dc8fc80eb1f
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-03 09:03:02 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -298,8 +298,10 @@ btns.forEach((btn) =>{
   tl.to(bar, {xPercent:0})
     .addPause("exit")
   exitTime = tl.duration()
-  tl.to(bar, {x:'100%'}, 0)
-  tl.to(arrow, {xPercent:10}, 0)
+  tl.to(bar, {x:'100%'})
+  tl.to(arrow, {x:'1rem'})
+    
+ 
   
   btn.addEventListener("mouseenter", () => {
     if(tl.time() < exitTime){
