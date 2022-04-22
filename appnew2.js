929bc1191fc5212e41fa076a472a2509be82423d
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-22 17:21:57 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -561,11 +561,13 @@ ScrollTrigger.create({
 const showAnim = gsap.from('.header', { 
   yPercent: -100,
   paused: true,
-  duration: 0.2
+  duration: 0.6
 }).progress(1);
 
 ScrollTrigger.create({
-  start: "top 600px",
+  trigger: ".section-home-kingdome-are",
+  scroller: ".smooth-scroll",
+  start: "top top",
   end: 99999,
   onUpdate: (self) => {
     self.direction === -1 ? showAnim.play() : showAnim.reverse()
