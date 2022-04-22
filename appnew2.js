0e1108e0a316fac22dacecfaadde79156bd30480
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-22 19:23:28 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -689,8 +689,7 @@ function initLoader() {
     /* .to(fadeintxt, {autoAlpha: 1, ease: 'power1.inOut'}) */
    .from(targets, {opacity:0, yPercent: 100, ease: "expo.inOut", stagger: 0.15})
    .from(txtdonji, {opacity:0, yPercent: 100, ease: "expo.inOut"}, "+=0.2")
-   .to(".homeani1", {delay:3, autoAlpha:1, stagger: 0.12, ease: "expo.inOut", })
-   .from(".header_redflag", { yPercent:-100, ease:'expo.inOut'}, "<")
+  
 
   
 
@@ -715,6 +714,8 @@ function initLoader() {
    /*  .to(lines, {yPercent: -500, stagger: 0.2}, 0) */
     .to([loader, loaderContent], {yPercent: -100}, 0)
     .to(fadeintxt, {autoAlpha: 0, ease: 'power1.inOut'})
+    .to(".homeani1", {delay:3, autoAlpha:1, stagger: 0.12, ease: "expo.inOut", })
+    .from(".header_redflag", { yPercent:-100, ease:'expo.inOut'}, "<")
     //.to('.main', {y: 0}, 0);
 
   const tlLoader = gsap.timeline();
