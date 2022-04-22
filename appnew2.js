7b15590c20c5b9d46e41cc94d17a57fee8aca6b2
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-22 19:25:40 +0200

    ht

--- appnew2.js
+++ appnew2.js
@@ -715,7 +715,7 @@ function initLoader() {
     .to([loader, loaderContent], {yPercent: -100}, 0)
     .to(fadeintxt, {autoAlpha: 0, ease: 'power1.inOut'})
     .to(".homeani1", {autoAlpha:1, stagger: 0.12, ease: "expo.inOut"}, 0.1)
-    .from(".header_redflag", { yPercent:-100, ease:'expo.inOut'})
+    .from(".header_redflag", { yPercent:-100, ease:'expo.inOut'}, 0.1)
     //.to('.main', {y: 0}, 0);
 
   const tlLoader = gsap.timeline();
