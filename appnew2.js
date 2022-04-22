9df68200cd8864ec82f4e5181f13e23c558ea890
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-22 18:54:58 +0200

    jy

--- appnew2.js
+++ appnew2.js
@@ -686,7 +686,7 @@ function initLoader() {
     /* .to(loaderInner, {scaleY: 1, transformOrigin: 'bottom', ease: 'power1.inOut'}) */
     /* .to(fadeintxt, {autoAlpha: 1, ease: 'power1.inOut'}) */
    .from(targets, {opacity:0, yPercent: 100, ease: "expo.inOut", stagger: 0.15})
-   .from(txtdonji, {opacity:0, yPercent: 100, ease: "expo.inOut"}, "<1")
+   .from(txtdonji, {opacity:0, yPercent: 100, ease: "expo.inOut"}, "<3")
 
 
 
