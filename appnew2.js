31a55c3d8e06c5f439dba3b04bd2c6833f3c2c99
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-04-22 17:38:49 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -679,7 +679,7 @@ function initLoader() {
   tlLoaderOut
 
     .to(lines, {yPercent: -500, stagger: 0.2}, 0)
-    .to([loader, loaderContent], {yPercent: -100}, 0.2)
+    .to([loader, loaderContent], {autoAlpha:0}, 0.2)
     //.to('.main', {y: 0}, 0);
 
   const tlLoader = gsap.timeline();
