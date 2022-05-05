2d3c4a401c881a0de56ca25cd71596067e90274c
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-05 10:14:56 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -265,7 +265,7 @@ HOME - ROTATE WIREFRAME
       scrub:0.2,
       start: 'top 80%',
       end:'+=10000',
-      transformOrigin:"center center"
+      transformOrigin:"center center",
       invalidateOnRefresh:true
     }
   })
