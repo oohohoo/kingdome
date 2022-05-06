0b51df3e3994d21022a75fe188a445dc5244a2a8
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-06 03:33:19 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -289,7 +289,7 @@ const lightbox = document.querySelector(".lightbox");
 
 const swiper = new Swiper(slider, {
   spaceBetween: 16,
-  slidesPerView: "auto",
+  slidesPerView: "2.3",
   //centeredSlides: true,
   loopedSlides: slides.length,
   loop: true,
