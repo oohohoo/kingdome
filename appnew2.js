d58a8b021a366fa0c1a6da4e3f74730f155ed9a3
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-07 10:58:33 +0200

    fe

--- appnew2.js
+++ appnew2.js
@@ -1871,94 +1871,13 @@ function homeAboutSlider() {
 
 
 
-/*
-================================================================================
-PROJECT MAIN VERTICAL SWIPER
-================================================================================
-*/
-function projectMainSwiper() {
-  /* AKAPOWL GALLERY*/
-  
-  
-
-
-
-var mySwiper = new Swiper ('.swiper-container', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-  speed: 1000,
-  grabCursor: true,
-  mousewheel: true,
-
-  effect: "slide",
-  autoplay: true,
-  parallax: true,
-  watchSlidesProgress: true, // turn off for horizontal
-  mousewheelControl: 1,
-
-
-
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    type: 'bullets',
-    clickable: true,
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  on: {
-    slideChangeTransitionStart: function () {
-        // Slide captions
-        var swiper = this;
-        setTimeout(function () {
-          var currentTitle = $(swiper.slides[swiper.activeIndex]).attr("data-title");
-          var currentSubtitle = $(swiper.slides[swiper.activeIndex]).attr("data-subtitle");
-        }, 500);
-        gsap.to($(".current-title"), 0.4, {autoAlpha: 0, y: -40, ease: Power1.easeIn});
-        gsap.to($(".current-subtitle"), 0.4, {autoAlpha: 0, y: -40, delay: 0.15, ease: Power1.easeIn});
-    },
-    slideChangeTransitionEnd: function () {
-        // Slide captions
-        var swiper = this;
-        var currentTitle = $(swiper.slides[swiper.activeIndex]).attr("data-title");
-        var currentSubtitle = $(swiper.slides[swiper.activeIndex]).attr("data-subtitle");
-        $(".slide-captions").html(function() {
-          return "<h2 class='current-title'>" + currentTitle + "</h2>" + "<h3 class='current-subtitle'>" + currentSubtitle + "</h3>";
-        });
-        gsap.from($(".current-title"), 0.4, {autoAlpha: 0, y: 40, ease: Power1.easeOut});
-        gsap.from($(".current-subtitle"), 0.4, {autoAlpha: 0, y: 40, delay: 0.15, ease: Power1.easeOut});
-    }
-  }
-});
-
-// Slide captions
-var currentTitle = $(mySwiper.slides[mySwiper.activeIndex]).attr("data-title");
-var currentSubtitle = $(mySwiper.slides[mySwiper.activeIndex]).attr("data-subtitle");
-$(".slide-captions").html(function() {
-return "<h2 class='current-title'>" + currentTitle + "</h2>" + "<h3 class='current-subtitle'>" + currentSubtitle + "</h3>";
-});
-
-
-
-  
-  }
-  
-
-
 
 /*
 ================================================================================
 PRODUCTS - FULLSCREEN SWIPER
 ================================================================================
 */
-function productsMainSwiper() {
+function projectMainSwiper() {
 
   const slider = document.getElementById("js-cta-slider");
   const sliderCounter = document.getElementById("js-cta-slider-counter");
