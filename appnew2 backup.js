f4c13e5c2b210d5660387b595b11267fea74a551
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-07 10:26:46 +0200

    g

--- appnew2 backup.js
+++ appnew2 backup.js
@@ -1502,6 +1502,50 @@ $( "#totop" ).on( "click", function() {
 	});
 }
 
+/*
+================================================================================
+PRODUCTS - FULLSCREEN SWIPER
+================================================================================
+*/
+function productsMainSwiper() {
+
+  const slider = document.getElementById("js-cta-slider");
+  const sliderCounter = document.getElementById("js-cta-slider-counter");
+  const sliderNext = document.getElementById("js-cta-slider-next");
+  const sliderPrevious = document.getElementById("js-cta-slider-previous");
+  
+  const interleaveOffset = 0.75;
+  
+    
+  
+  // svaka fotka ima: data-swiper-parallax-y: "35%"
+  
+  const swiper = new Swiper(slider, {
+    autoplay: false,
+    parallax: true,
+    loop: true,
+    effect: "slide",
+    direction: "vertical", // put horizontal
+    speed: 1000,
+    grabCursor: true,
+    watchSlidesProgress: true, // turn off for horizontal
+    //mousewheelControl: true,
+    mousewheelControl: 1,
+    mousewheel: true,
+    pagination: {
+      el: sliderCounter,
+      type: "custom",
+      renderCustom: function(swiper, current, total) {
+        let i = current ? current : 0;
+        return `${("0" + i).slice(-2)} / ${("0" + total).slice(-2)}`;
+      }
+    },
+    navigation: {
+      nextEl: sliderNext,
+      prevEl: sliderPrevious
+    },
+  });
+}
 
 /*
 ================================================================================
@@ -1887,18 +1931,9 @@ var mySwiper = new Swiper ('.swiper-container', {
   // Optional parameters
   direction: 'vertical',
   loop: true,
-  speed: 1000,
+  speed: 1200,
   grabCursor: true,
   mousewheel: true,
-
-  effect: "slide",
-  autoplay: false,
-  parallax: true,
-  watchSlidesProgress: true, // turn off for horizontal
-  mousewheelControl: 1,
-
-
-
 
   // If we need pagination
   pagination: {
@@ -1952,48 +1987,3 @@ return "<h2 class='current-title'>" + currentTitle + "</h2>" + "<h3 class='curre
   
 
 
-
-/*
-================================================================================
-PRODUCTS - FULLSCREEN SWIPER
-================================================================================
-*/
-function productsMainSwiper() {
-
-  const slider = document.getElementById("js-cta-slider");
-  const sliderCounter = document.getElementById("js-cta-slider-counter");
-  const sliderNext = document.getElementById("js-cta-slider-next");
-  const sliderPrevious = document.getElementById("js-cta-slider-previous");
-  
-  const interleaveOffset = 0.75;
-  
-    
-  
-  // svaka fotka ima: data-swiper-parallax-y: "35%"
-  
-  const swiper = new Swiper(slider, {
-    autoplay: false,
-    parallax: true,
-    loop: true,
-    effect: "slide",
-    direction: "vertical", // put horizontal
-    speed: 1000,
-    grabCursor: true,
-    watchSlidesProgress: true, // turn off for horizontal
-    //mousewheelControl: true,
-    mousewheelControl: 1,
-    mousewheel: true,
-    pagination: {
-      el: sliderCounter,
-      type: "custom",
-      renderCustom: function(swiper, current, total) {
-        let i = current ? current : 0;
-        return `${("0" + i).slice(-2)} / ${("0" + total).slice(-2)}`;
-      }
-    },
-    navigation: {
-      nextEl: sliderNext,
-      prevEl: sliderPrevious
-    },
-  });
-}
