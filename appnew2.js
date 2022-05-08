c47a17b4656ea8fa44ca0e5e73528b2d4615c09c
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-08 18:01:16 +0200

    g

--- appnew2.js
+++ appnew2.js
@@ -354,7 +354,7 @@ function pageTransitionIn({
     .set(loaderInner, {autoAlpha: 0})
     .fromTo(loader, {yPercent: -100}, {yPercent: 0})
     .fromTo(loaderMask, {yPercent: 80}, {yPercent: 0}, 0)
-    .to(container, {y: 150}, 0);
+   // .to(container, {y: 150}, 0);
 
   return tl;
 }
@@ -376,7 +376,7 @@ function pageTransitionOut({
   tl
     .to(loader, {yPercent: 100})
     .to(loaderMask, {yPercent: -80}, 0)
-    .from(container, {y: -150}, 0);
+   // .from(container, {y: -150}, 0);
   return tl;
 }
 
