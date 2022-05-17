b3d0f5bd1bb76b02af4c276b9cacfe437629bba7
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-17 21:14:57 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -1249,7 +1249,7 @@ function logoTransformOnScroll() {
 
   .to(".header_redflag",  {width:'3rem', height:'3rem', top: '0.5rem', duration: 0.7, ease: "expo.inOut", }, 0)
   .to("#di", {morphSVG: {shape: "#sq"}, duration: 0.7, ease: "expo.inOut"}, 0)
-  .to(".header_znak", { scale: 0.7, duration: 0.7, transformOrigin: 'center center', yPercent: -40, ease:'expo.inOut'}, 0)
+  .to(".header_znak", { scale: 0.7, duration: 0.7, transformOrigin: 'center center', yPercent: -50, ease:'expo.inOut'}, 0)
   .to(".header_kingdome", { yPercent: 20, ease:'expo.inOut'}, 0)
 
 /*   .to("#ticker",  {autoAlpha:0, duration: 0.1}); */
