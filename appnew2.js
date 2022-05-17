9812d216bad5881a3688a9c0344d9574a514e592
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-05-17 17:56:55 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -1371,7 +1371,7 @@ function showModal(){
 
   
 let modal = gsap.timeline();
-  modal.to("#popup'", { autoAlpha: 1, rotate: 360, ease:'none'}, 0)
+  modal.to("#popup", { autoAlpha: 1, rotate: 360, ease:'none'}, 0)
 
 
   sessionStorage.setItem('alreadyShow','alredy shown');
