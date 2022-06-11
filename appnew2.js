65e19ade5184cdb1784edc065c7e30a64e870a6b
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-11 07:19:44 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -2154,10 +2154,10 @@ function slideInHeaders() {
 
 tl.from(mySplitText.chars, {
   duration: 1.2,
-  //skewY: 20,
- // rotationX: "+=90",
- // opacity: 0,
-  y: 100,
+  skewY: 20,
+  rotationX: "+=90",
+  opacity: 0,
+  y: -100,
   ease: "quart.inOut",
   stagger: 0.025
 });
