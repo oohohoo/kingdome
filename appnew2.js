aa906bf870b52d3595cec6179f17dbba110ccc96
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-13 19:46:19 +0200

    g

--- appnew2.js
+++ appnew2.js
@@ -862,7 +862,7 @@ function show() {
 
  
 
-	tl.to(".nav-wrapper", {transformOrigin: "top center", duration: 1, height: "100%"})
+	tl.to(".nav-wrapper", {transformOrigin: "bottom center", duration: 1, height: "100%"})
     .to(".fs-menu--column", {yPercent:0, duration:0.8, ease: "quart.inOut"}, "<0.1")
     .to(".fs-menu-header", {yPercent:0, duration:0.8, ease: "quart.inOut"}, "<0.2")
    
@@ -888,7 +888,7 @@ function hide() {
 
 		tl.to(".fs-nav-item", {autoAlpha:1, duration:0.5,stagger:0.05,  ease: "quart.out"}, "<0.1")
     .fromTo(".fs-menu--column", {yPercent:0}, {yPercent:-100, duration:0.4, stagger:0.05, ease: "quart.out"})
-		.to(".nav-wrapper", { duration: 0.1, transformOrigin: "top center", height: "0%"})
+		.to(".nav-wrapper", { duration: 0.1, transformOrigin: "bottom center", height: "0%"})
 		.to(".open", {autoAlpha:1}, "<")
 		.to(".close", {autoAlpha:0}, "<")
     .set(" .nav-toggle", { pointerEvents: "all"});
