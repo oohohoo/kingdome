0f3a3c16deb1cc02b3cc81863fe3d9256a76a2a7
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-13 16:50:21 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -1278,11 +1278,13 @@ function fadeInOnEnter() {
       ease: "expo.out",
     })
   }); */
-
+  gsap.set('.batchzero', {
+    opacity: 0,
+  });
 
   ScrollTrigger.batch('.card', {
     onEnter: batch => {
-      batch.forEach((card/* , index */) => gsap.to(card.children, {autoAlpha:1, stagger: 0.125/* , delay: index * 0.3 */}))
+      batch.forEach((card/* , index */) => gsap.to(card.children, {opacity:1, stagger: 0.125/* , delay: index * 0.3 */}))
     },
     /* once: true */
   });
