1ef1605a230bf26e8ac296468ff89ed0ea243801
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-15 12:17:03 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -866,12 +866,12 @@ function show() {
 		{duration: 0.5, scaleY: 1, ease: "Expo.inOut",})
     
 
-   .to(".xnav", {scaleY: 1, transformOrigin: "top center", duration:0.6, ease: "quart.inOut"}) 
-    //.to(".fs-menu--column", {yPercent:0, duration:0.8, ease: "quart.inOut"}, "<")
-    //.to(".fs-menu-header", {yPercent:0, duration:0.8, ease: "quart.inOut"}, "<0.2")
+   .to(".xnav", {scaleY: 1, transformOrigin: "top center", duration:0.1, ease: "quart.inOut"}) 
+    .to(".fs-menu--column", {yPercent:0, duration:0.8, ease: "quart.inOut"}, "<")
+    .to(".fs-menu-header", {yPercent:0, duration:0.8, ease: "quart.inOut"}, "<0.2")
    
-   // .to(".fs-nav-item", {autoAlpha:1, duration:0.5,stagger:0.05,  ease: "quart.inOut"}, "<0.1")
-   // .to(".fadein", {autoAlpha:1, duration:0.3, ease: "quart.inOut"}, "<0.1")
+    .to(".fs-nav-item", {autoAlpha:1, duration:0.5,stagger:0.05,  ease: "quart.inOut"}, "<0.1")
+    .to(".fadein", {autoAlpha:1, duration:0.3, ease: "quart.inOut"}, "<0.1")
 
 
 		.to(openmenu, {autoAlpha:0}, "<")
