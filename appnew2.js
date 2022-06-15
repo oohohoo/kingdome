b2be649af1d6eb333cd9ae14de70822288d81f2a
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-15 12:44:32 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -901,7 +901,7 @@ function hide() {
 
 
 
-	//	.to(".fs-nav-item", {autoAlpha:1, duration:0.5,stagger:0.05,  ease: "quart.inOut"}, "<0.1")
+		.to(".fs-nav-item", {autoAlpha:0, duration:0.5,stagger:0.05,  ease: "quart.inOut"}, "<0.1")
   //  .fromTo(".fs-menu--column", {yPercent:0}, {yPercent:-100, duration:0.4, stagger:0.05, ease: "quart.inOut"})
 	/* 	.to(".xnav", { duration: 0.1, transformOrigin: "top center", scaleY: 0}) */
 		.to(openmenu, {autoAlpha:1}, "<")
