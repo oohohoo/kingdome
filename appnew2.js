1383012ced30c2fe637b827f4e0597a52b77840d
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-15 19:39:56 +0200

    ht

--- appnew2.js
+++ appnew2.js
@@ -1822,7 +1822,7 @@ function headerHide() {
   ScrollTrigger.create({
     trigger: ".navchange",
     scroller: ".smooth-scroll",
-    start: "top -10%",
+    start: "top -15%",
     end: 99999,
     toggleClass: { targets: ".headerx", className: "background-color-white" },
     //toggleClass: { targets: ".hamburger-lajna", className: "background-color-white" },
