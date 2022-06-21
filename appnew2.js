2468ff9082711085498764c79f7248c09ef7d8c9
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-21 19:54:20 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -1168,7 +1168,7 @@ function headerHide() {
     scroller: ".smooth-scroll",
     start: "top top",
     end: 99999,
-    toggleClass: { targets: ".header, .hamby-line, .w--current", className: "navcolor" },
+    toggleClass: { targets: ".header-back, .hamby-line, .w--current", className: "navcolor" },
     onUpdate: (self) => {
       self.direction === -1 ? showAnim.play() : showAnim.reverse()
     }
