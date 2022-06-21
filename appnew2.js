4d831f62a5492dd550a7d116b7308a11ebf60598
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-21 14:34:15 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -329,7 +329,7 @@ function initContent() {
 			parallaxPanel();
 			startStopVideo();
 			swiperSolo();
-		//	homePinSections();
+			homePinSections();
 			homeVideoClip();
 			headerHide();
 			toggleNavClass();
@@ -1399,6 +1399,71 @@ function swiperSolo() {
 	});
 }
 
+/*
+================================================================================
+SWIPER PRODUCT HOMEPAGE
+================================================================================
+*/
+
+function swiperCustomPaginationHome() {
+  var menu = ['01', '02', '03']
+  var swiper = new Swiper('.swiper-container', {
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (menu[index]) + '</span>';
+          },
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false
+    },
+    loop: true,
+    watchSlidesProgress: true
+  });
+  
+  // var swiperEl = document.querySelector('.swiper-container');
+  
+  // swiperEl.addEventListener('mouseenter', function(event) {
+  document.addEventListener('mouseenter', event => {
+    const el = event.target;
+    if (el && el.matches && el.matches('.swiper-container')) {
+      // console.log('mouseenter');
+      // console.log('autoplay running', swiper.autoplay.running);
+      el.swiper.autoplay.stop();
+      el.classList.add('swiper-paused');
+      
+      const activeNavItem = el.querySelector('.swiper-pagination-bullet-active');
+      activeNavItem.style.animationPlayState="paused";
+    }
+  }, true);
+  
+  document.addEventListener('mouseleave', event => {
+    // console.log('mouseleave', swiper.activeIndex, swiper.slides[swiper.activeIndex].progress);
+    // console.log('autoplay running', swiper.autoplay.running);
+    const el = event.target;
+    if (el && el.matches && el.matches('.swiper-container')) {
+      el.swiper.autoplay.start();
+      el.classList.remove('swiper-paused');
+  
+      const activeNavItem = el.querySelector('.swiper-pagination-bullet-active');
+      
+      activeNavItem.classList.remove('swiper-pagination-bullet-active');
+      // activeNavItem.style.animation = 'none';
+  
+      setTimeout(() => {
+        activeNavItem.classList.add('swiper-pagination-bullet-active');
+        // activeNavItem.style.animation = '';
+      }, 10);
+    }
+  }, true);
+  console.log("SWIPER CUSTOM PAGINATION");
+}
 
 
 /*
@@ -1428,6 +1493,26 @@ videos.forEach(function(video, i) {
 })
 }
 
+
+/*
+================================================================================
+HOME PIN SECTIONS / nema na home
+================================================================================
+*/
+
+function homePinSections() {
+
+
+gsap.utils.toArray(".picturewrap").forEach((panel, i) => {
+  ScrollTrigger.create({
+    scroller: ".smooth-scroll",
+    trigger: panel,
+    start: "top top", 
+    pin: true, 
+  });
+});
+
+}
 
 
 /*
