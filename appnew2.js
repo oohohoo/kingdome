c46804a9a112a8545230851b0b7d4edf3fcd7283
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-21 19:07:28 +0200

    fe

--- appnew2.js
+++ appnew2.js
@@ -1572,11 +1572,7 @@ tlout.to(".home-hero-video, .cta__slider", {
   });
 
 
-tl.to(".home-hero-video, .cta__slider", {
-  opacity:0.4,
-
-})
-
+tl.to(".home-hero-video, .cta__slider", {opacity:0.4,})
 
  .to(mySplitText.chars, {autoAlpha: 0, yPercent: 100, duration: 1, ease: "power2", 
     stagger: {
