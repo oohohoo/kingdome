d7a7fbda06715b9cece0dd6184332d33a61abc1f
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-21 11:06:50 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -2534,10 +2534,10 @@ function heroPanelAnimation() {
     //markers: true,
     id: "hero"
 }}); */
+
+const splitHeadings = headings.map(heading => new SplitText(heading, { type: "chars,words,lines", linesClass: "clip-text"})),
+
 gsap.set(".home-hero-video", {opacity:1})
-let splitHeadings = headings.map(heading => new SplitText(heading, { type: "chars,words,lines", linesClass: "clip-text"})),
-
-
 
 let tl = gsap.timeline({
   scrollTrigger: {
