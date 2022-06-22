e0467740459a08b08307b8eaad7a0b7ad0aa8516
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-22 10:15:13 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -703,7 +703,7 @@ tl.to(".xnav", {scaleY: 1, transformOrigin: "bottom center", duration:0.6, ease:
     clipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",
     webkitClipPath: "polygon(0 0, 100% 0, 100% 100%, 0% 100%)",  
     ease: "Expo.inOut",
-    },"<0.1")
+    },"<0.15")
    
     .from(".nav-image", {autoAlpha:0, scale:1.1}, "<0.2")
     
