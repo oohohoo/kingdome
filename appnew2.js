5ecf76c83ddc8d460d46b37a63a36d82e515b26e
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-22 20:31:10 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -1022,7 +1022,7 @@ function fadeInOnEnter() {
 		start: "top bottom-=100px",
 		onEnter: (batch) => {
 			batch.forEach((card, index) =>
-				gsap.to(card.children, { y: 0, autoAlpha: 1, stagger: 0.05 })
+				gsap.to(card.children, { y: 0, autoAlpha: 1, ease: 'hop', stagger: 0.05 })
 			);
 		},
 		once: true,
