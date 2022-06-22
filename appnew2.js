f074a778262ec0b8b7727c97813b73f5dff5ec88
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-22 10:09:53 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -691,10 +691,9 @@ function show() {
 tl.to(".xnav", {scaleY: 1, transformOrigin: "bottom center", duration:0.01}) 
 // UNUTARNJI  
     .fromTo(".nav--trans", {scaleY: 0, transformOrigin: "bottom center"},
-		{duration: 0.8, scaleY: 1, ease: "Expo.inOut"}, 0.01)
+		{duration: 0.8, scaleY: 1, ease: "Expo.inOut"}, 0.1)
   
-// UNUTARNJI 
-   // .to(".fs-menu--column", {autoAlpha:1, duration:0.1}, "<0.1")
+// IMAGE CLIP
    .fromTo(".clip", {
     clipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
     webkitClipPath: "polygon(0 100%, 100% 100%, 100% 100%, 0% 100%)",
