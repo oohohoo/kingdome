3e994ecc58a3810682ffc9785726b704ecf75172
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-27 18:11:58 +0200

    g

--- appnew2.js
+++ appnew2.js
@@ -1672,7 +1672,7 @@ function heroPanelAnimation() {
 //gsap.set(".home-hero-video", {opacity:1, scale:1.2})
 //gsap.set(".red-flag", {yPercent:100})
 if (document.querySelector('.smooth-scroll')) {
-locoScroll.stop();
+/* locoScroll.stop(); */
 
 console.log("LOCO STOPPED");
 
