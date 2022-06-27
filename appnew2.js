122f6b02de20e3418e5ab83d71d085de63d6f6c7
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-27 21:09:06 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -2035,7 +2035,7 @@ Webflow.push(function () {
 
 function vhFix() {
 
-const documentHeight = () => {
+const documentHeightX = () => {
   const doc = document.documentElement
   doc.style.setProperty('--doc-height', '${window.innerHeight}px')
   console.log("VH DOC HEIGHT");
