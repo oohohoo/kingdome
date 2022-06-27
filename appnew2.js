c78826ee77f5746cd767947027e87aee0da78f36
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-27 20:32:18 +0200

    hu

--- appnew2.js
+++ appnew2.js
@@ -169,12 +169,12 @@ window.addEventListener('resize', function(){
 */
 
 	
-/* let vh = window.innerHeight * 0.01;
+ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 window.addEventListener('resize', () => {
   let vh = window.innerHeight * 0.01;
   document.documentElement.style.setProperty('--vh', `${vh}px`);
-}); */
+}); 
 
 
 /*
