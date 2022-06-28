e03a3355b400ae99468613f68fc5e7728e2fd793
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-28 18:32:27 +0200

    ht

--- appnew2.js
+++ appnew2.js
@@ -1476,7 +1476,7 @@ function swiperSolo() {
 	var swipersolo = new Swiper(".swiper-container-solo", {
 		loopedSlides: 6,
 		loop: true,
-		spaceBetween: 30,
+		spaceBetween: 60,
 		slidesPerView: 1,
 		freeMode: true,
 		grabCursor: true,
