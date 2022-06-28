a5db42663a2a1e54a85c1798d8a3dd7184ffd9d7
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-28 14:24:56 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -327,6 +327,7 @@ function initContent() {
 		},
 		contact: function () {
 			webflowInteractions();
+      contactSliderFix();
 			//  openMobileMenu();
 		},
 		faq: function () {
@@ -2053,3 +2054,30 @@ window.addEventListener('resize', () => {
  */
 }
 
+
+/*
+================================================================================
+CONTACT SLIDER HEIGHT FIX TO AUTO
+================================================================================
+*/
+
+function contactSliderFix() {
+const w_slides = document.querySelectorAll(".w-slide");
+
+for (var i = 0; i < w_slides.length; i++) {
+	const observer = new MutationObserver(function (mutation) {
+		var w_slides = document.querySelectorAll(".w-slide");
+
+		for (var i = 0; i < w_slides.length; i++) {
+			if (!w_slides[i].hasAttribute("aria-hidden")) {
+				var slideSize = w_slides[i].clientHeight;
+
+				w_slides[i].closest(".w-slider").style.height = slideSize + "px";
+			}
+		}
+	});
+
+	observer.observe(w_slides[i], { attributes: true });
+}
+	
+}
