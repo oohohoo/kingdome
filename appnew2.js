e96b86f1bd3d9a2d417cb9c3e81d31607be28d73
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-29 19:51:35 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -1170,7 +1170,7 @@ var switchlogosmall = document.querySelector(".close-wrap");
   }
   var doSwitchOut = function(shape, show){
     var TL2 = gsap.timeline({ defaults: {duration: 0.3, overwrite: 'auto', force3D:false, ease: "power1.in"} })
-    .to('#switch', {autoAlpha: show, duration: 0.3},0-0.2 )
+    .to('#switch', {autoAlpha: show, duration: 0.3},0 )
     .to('#start', {morphSVG:shape},0)
      .to(".header_znak", { scale: 1, yPercent: 0}, 0) 
    .to(".h-red-flag",  {width:'6rem', height:'9rem', top: '0rem'}, 0) 
