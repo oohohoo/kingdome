55c6595c3c3045e3ae8c4452d8b999aabfac11b4
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-06-29 10:34:17 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -1150,7 +1150,7 @@ function logoTransformOnScroll() {
     scrollTrigger: {
       scroller: ".smooth-scroll",
         trigger: "#start",
-        start: "top top", // when the top of the trigger hits the top of the viewport
+        start: "10% top", // when the top of the trigger hits the top of the viewport
         end: "+=10000000", // end after scrolling 500px beyond the start
         toggleActions: 'play reverse play reverse',
         invalidateOnRefresh: true,
