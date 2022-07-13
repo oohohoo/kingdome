e32dc9b57d7015ad7ef6645ad1fb5e683a9f3e52
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-13 19:37:13 +0200

    ht

--- appnew2.js
+++ appnew2.js
@@ -1527,7 +1527,7 @@ function logoMarquee() {
 
   //Change width and time on your desire
 
-initMarquee(250, 70)
+initMarquee(223, 99)
 
 function initMarquee(boxWidth, time) {
     const boxElement = $('.logo-box');
