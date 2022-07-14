9a17777f39b64640c0e1f2fea131a8ed6bb22a6b
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-14 20:29:23 +0200

    ht

--- appnew2.js
+++ appnew2.js
@@ -1479,7 +1479,7 @@ function productsTabs() {
       // animate bubble slider to clicked target
       animation.to(".slider-tab", {x:targets[activeTab].offsetLeft, width:targets[activeTab].offsetWidth});
       // change text color on old and new tab targets
-      animation.to(targets[old], {color:"#0f3936", opacity:.57, ease:"none"}, 0);
+      animation.to(targets[old], {color:"#ff0000", opacity:.57, ease:"none"}, 0);
       animation.to(targets[activeTab], {color:"#0f3936", ease:"none"}, 0);
       // slide current article down out of view and then set it to starting position at top
       animation.to(articles[old], {y:0, zIndex:1, opacity:0, ease:"hop" }, 0);
