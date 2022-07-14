c3caed1199f81222e734d824e0a8d4ef4a729d03
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-15 01:37:29 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -1878,7 +1878,7 @@ tlin.fromTo(mySplitText.words, { transformOrigin: "bottom left", autoAlpha:0, yP
   }
 });
 
-tlout.to(".home-hero-video", { yPercent:-10, duration: 0.3 }, 0) 
+tlout.to(".home-hero-video", { yPercent:-20,  duration: 0.8 }, 0) 
 
 
 /* .fromTo(mySplitText.words, { transformOrigin: "bottom left", yPercent: 0, rotateZ: 0}, {
