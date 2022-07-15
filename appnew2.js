600b752d5964845cb5e4521735098bb7e8b67639
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-15 02:25:29 +0200

    rg

--- appnew2.js
+++ appnew2.js
@@ -1879,7 +1879,7 @@ tlin.fromTo(mySplitText.words, { transformOrigin: "bottom left", autoAlpha:0, yP
 });
 
 tlout.to(".home-hero_video-wrap", { yPercent:-20,  duration: 0.8 }, 0) 
-.to(".home-hero_head-wrap", { yPercent:30,   duration: 0.6 }, 0.3) 
+.to(".home-hero_head-wrap", { yPercent:30,   duration: 0.6 }, 0) 
 
 /* // PIN 2
 var tldue = gsap.timeline({
