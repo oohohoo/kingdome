39b5315d642f48456c7c2002af755ac14e1beb52
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-17 09:37:44 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -1547,7 +1547,7 @@ function productsTabs() {
   // set initial article and position bubble slider on first tab 
   gsap.set(articles[0], {y:0, opacity:1, zIndex:2});
   gsap.set(".slider-tab", {x:targets[0].offsetLeft, width:targets[0].offsetWidth});
-  gsap.set(targets[0], {color:"#0f3936, opacity:1"});
+  gsap.set(targets[0], {color:"#0f3936", opacity:0});
   gsap.set(".article-block", {height:heights[0]});
   
   function doCoolStuff() {
