8ecd24eb5402eeabe84ac84bf6654bc59d7cf1ee
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-17 16:56:14 +0200

    gr

--- appnew2.js
+++ appnew2.js
@@ -1968,7 +1968,7 @@ mySplitText = new SplitText(".head-split", {
 });
 
 
-tlin.fromTo(mySplitText.words, { transformOrigin: "bottom left", autoAlpha:0, yPercent: 100, rotateZ: 20}, {
+tlin.fromTo(mySplitText.words, { transformOrigin: "bottom left", yPercent: 100, rotateZ: 20}, {
  
   yPercent: 0,
   duration: 1,
