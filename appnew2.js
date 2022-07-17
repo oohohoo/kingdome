8276685a8f094fc2f1a4c1e3078620953966b7a0
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-17 18:45:00 +0200

    fe

--- appnew2.js
+++ appnew2.js
@@ -2361,3 +2361,65 @@ var tlfull = gsap.timeline({
 //tlfull.to(".home-full-image-wrap", { autoAlpha:0, duration: 0.6 }, 0) 
 
 }
+
+
+/*
+================================================================================
+SKETCHFAB VIEWER
+================================================================================
+*/
+
+function contactMobilePin() {
+
+// Sketchfab Viewer API: Start/Stop the viewer
+var version = '1.12.1';
+var uid = 'dd958716be0b4786b8700125eec618e5';
+var iframe = document.getElementById('api-frame');
+var client = new window.Sketchfab(version, iframe);
+
+var error = function error() {
+  console.error('Sketchfab API error');
+};
+
+var success = function success(api) {
+  api.addEventListener('viewerstart', function () {
+    console.log('viewerstart');
+  });
+  api.addEventListener('viewerstop', function () {
+    console.log('viewerstop');
+  });
+  api.start(function () {
+    document.getElementById('start').addEventListener('click', function () {
+      api.start();
+    });
+    document.getElementById('stop').addEventListener('click', function () {
+      api.stop();
+    });
+    api.addEventListener('viewerready', function () {
+      console.log('viewerReady');
+    });
+  });
+};
+
+client.init(uid, {
+  success: success,
+  error: error,
+  autostart: 1,
+  preload: 1
+}); //////////////////////////////////
+// GUI Code
+//////////////////////////////////
+
+function initGui() {
+  var controls = document.getElementById('controls');
+  var buttonsText = '';
+  buttonsText += '<button id="start">Start</button>';
+  buttonsText += '<button id="stop">Stop</button>';
+  controls.innerHTML = buttonsText;
+}
+
+initGui(); //////////////////////////////////
+// GUI Code end
+//////////////////////////////////
+
+}
