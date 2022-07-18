b45ce5e461027d93128702008ed43b2cf29af652
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-18 13:32:50 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -313,7 +313,7 @@ function initContent() {
 		},
 		home: function () {
    //  logoTransformOnScroll();
-      heroPanelAnimation();
+    //  heroPanelAnimation();
 		//	startStopVideo();
 		//	headerHide();
      // akapowPinned();
@@ -363,13 +363,13 @@ function initContent() {
 
 	// LOAD THIS SCRIPTS ON EVERY PAGE
 	initScroll();
-  fullscreenMenu();
+ /*  fullscreenMenu();
   buttonHoverFromDirection();
 	scrollToTop();
 	yearUpdate();
 	fadeInOnEnter();
 	cubertoCursor();
-  disableScroll();
+  disableScroll(); */
   /* vhFix(); */
 
 	myscripts.init();
