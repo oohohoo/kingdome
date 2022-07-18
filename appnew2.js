a8c394f2e5fbb98892683996b2821a77d126e449
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-18 15:12:05 +0200

    ht

--- appnew2.js
+++ appnew2.js
@@ -1949,7 +1949,7 @@ gsap.set(".scrolldown", {yPercent:-100})
 			start: "top top",
 			end: "bottom top",
 			scrub: true,
-			pin: ".section-home-hero",
+			pin: ".home-hero_video-wrap",
 			pinSpacing: false,
 			anticipatePin: 1,
 			toggleActions: "restart none none none",
