c72af558c3338f5b02e43fadaecaad81404c40f0
oohohoo”git config --global user.email dejankutic@me.comgit config --global user.name oohohoo” <dejankutic@me.com>
2022-07-18 13:35:46 +0200

    f

--- appnew2.js
+++ appnew2.js
@@ -367,7 +367,7 @@ function initContent() {
   buttonHoverFromDirection();
 //	scrollToTop();
 //	yearUpdate();
-//	fadeInOnEnter();
+	fadeInOnEnter();
 //	cubertoCursor();
  // disableScroll(); 
   /* vhFix(); */
